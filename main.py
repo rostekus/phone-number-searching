185015a04108b118124c74c82ce5506550120e08
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2021-03-27 12:17:01 +0100

    modified menu

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def new_file():
 
 def save_file():
     pass
+
+def open_file():
+    pass
+
+def rotate_left():
+    pass
+
+def rotate_right():
+    pass
+
+
 #=========================
 
 # Menu
@@ -24,13 +35,20 @@ my_menu = Menu(root)
 root.config(menu = my_menu)
 
 file_menu =Menu(my_menu)
+edit_menu = Menu(my_menu)
 
-my_menu.add_cascade(label='File',menu=file_menu)
+my_menu.add_cascade(label='File', menu = file_menu)
+my_menu.add_cascade(label='Edit',menu = edit_menu)
 #=========================
 file_menu.add_command(label='New',command = new_file)
 file_menu.add_command(label= 'Open...', command = open_file)
 file_menu.add_command(label= 'Save', command = save_file)
 file_menu.add_command(label= 'Exit', command = root.quit)
+
+edit_menu.add_cascade(label = 'Rotate right', command= rotate_left)
+edit_menu.add_cascade(label = 'Rotate left', command = rotate_left)
+
+
 
 #=========================
 
